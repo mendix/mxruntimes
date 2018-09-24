5b7a8434e26ad0f9a33beb5fa15e68777aaad3af
Mendix Runtime Packer <yolo@mendix.com>
2018-09-24 11:55:09 +0200

    add 7.19.0.40423

--- modeler/tools/node/node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/IReactView.java
+++ modeler/tools/node/node_modules/react-native-navigation/lib/android/app/src/main/java/com/reactnativenavigation/viewcontrollers/IReactView.java
@@ -4,6 +4,9 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.reactnativenavigation.interfaces.ScrollEventListener;
+import com.reactnativenavigation.views.element.Element;
+
+import java.util.List;
 
 public interface IReactView extends Destroyable {
 
@@ -20,4 +23,8 @@ public interface IReactView extends Destroyable {
     ScrollEventListener getScrollEventListener();
 
     void dispatchTouchEventToJs(MotionEvent event);
+
+    boolean isRendered();
+
+    List<Element> getElements();
 }
