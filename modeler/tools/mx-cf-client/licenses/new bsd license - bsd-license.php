4ed36fdc4ecb34222c1f43b65de8c15028fa1dfe
Mendix Runtime Packer <yolo@mendix.com>
2019-10-17 15:19:05 +0200

    add 6.10.19

--- modeler/tools/mx-cf-client/licenses/new bsd license - bsd-license.php
+++ modeler/tools/mx-cf-client/licenses/new bsd license - bsd-license.php
@@ -2,6 +2,6 @@
 <head><title>301 Moved Permanently</title></head>
 <body bgcolor="white">
 <center><h1>301 Moved Permanently</h1></center>
-<hr><center>nginx/1.10.3 (Ubuntu)</center>
+<hr><center>nginx/1.14.0 (Ubuntu)</center>
 </body>
 </html>
